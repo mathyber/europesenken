b2985b2ae1113bf726171b5347240a6961c67197
mathyber <ilan23f@gmail.com>
2023-03-22 00:38:21 +0300

    add songs snippets

--- src/constants/songs.tsx
+++ src/constants/songs.tsx
@@ -2,6 +2,41 @@ import {ISongData} from "../types/types";
 import audio1 from '../audio/1.mp3';
 import audio2 from '../audio/2.mp3';
 import audio3 from '../audio/3.mp3';
+import audio4 from '../audio/4.mp3';
+import audio5 from '../audio/5.mp3';
+import audio6 from '../audio/6.mp3';
+import audio7 from '../audio/7.mp3';
+import audio8 from '../audio/8.mp3';
+import audio9 from '../audio/9.mp3';
+import audio10 from '../audio/10.mp3';
+import audio11 from '../audio/11.mp3';
+import audio12 from '../audio/12.mp3';
+import audio13 from '../audio/13.mp3';
+import audio14 from '../audio/14.mp3';
+import audio15 from '../audio/15.mp3';
+import audio16 from '../audio/16.mp3';
+import audio17 from '../audio/17.mp3';
+import audio18 from '../audio/18.mp3';
+import audio19 from '../audio/19.mp3';
+import audio20 from '../audio/20.mp3';
+import audio21 from '../audio/21.mp3';
+import audio22 from '../audio/22.mp3';
+import audio23 from '../audio/23.mp3';
+import audio24 from '../audio/24.mp3';
+import audio25 from '../audio/25.mp3';
+import audio26 from '../audio/26.mp3';
+import audio27 from '../audio/27.mp3';
+import audio28 from '../audio/28.mp3';
+import audio29 from '../audio/29.mp3';
+import audio30 from '../audio/30.mp3';
+import audio31 from '../audio/31.mp3';
+import audio32 from '../audio/32.mp3';
+import audio33 from '../audio/33.mp3';
+import audio34 from '../audio/34.mp3';
+import audio35 from '../audio/35.mp3';
+import audio36 from '../audio/36.mp3';
+import audio37 from '../audio/37.mp3';
+
 export const songsArray: ISongData[] = [
     {
         id: 1,
@@ -15,251 +50,251 @@ export const songsArray: ISongData[] = [
         artist: 'Brunette',
         name: 'Future Lover',
         country: 'Armenia',
-        audio: audio3
+        audio: audio2
     },
     {
         id: 3,
         artist: 'Voyager',
         name: 'Promise',
         country: 'Australia',
-        audio: audio2
+        audio: audio3
     },
     {
         id: 4,
         artist: 'Teya & Salena',
         name: 'Who The Hell Is Edgar?',
         country: 'Austria',
-        audio: audio2
+        audio: audio4
     },
     {
         id: 5,
         artist: 'TuralTuranX',
         name: 'Tell Me More',
         country: 'Azerbaijan',
-        audio: audio2
+        audio: audio5
     },
     {
         id: 6,
         artist: 'Gustaph',
         name: 'Because Of You',
         country: 'Belgium',
-        audio: audio2
+        audio: audio6
     },
     {
         id: 7,
         artist: 'Let 3',
         name: 'Mama ŠČ!',
         country: 'Croatia',
-        audio: audio2
+        audio: audio7
     },
     {
         id: 8,
         artist: 'Andrew Lambrou',
         name: 'Break A Broken Heart',
         country: 'Cyprus',
-        audio: audio2
+        audio: audio8
     },
     {
         id: 9,
         artist: 'Vesna',
         name: 'My Sister’s Crown',
         country: 'Czechia',
-        audio: audio2
+        audio: audio9
     },
     {
         id: 10,
         artist: 'Reiley',
         name: 'Breaking My Heart',
         country: 'Denmark',
-        audio: audio2
+        audio: audio10
     },
     {
         id: 11,
         artist: 'Alika',
         name: 'Bridges',
         country: 'Estonia',
-        audio: audio2
+        audio: audio11
     },
     {
         id: 12,
         artist: 'Käärijä',
         name: 'Cha Cha Cha',
         country: 'Finland',
-        audio: audio2
+        audio: audio12
     },
     {
         id: 13,
         artist: 'La Zarra',
         name: 'Évidemment',
         country: 'France',
-        audio: audio2
+        audio: audio13
     },
     {
         id: 14,
         artist: 'Iru',
         name: 'Echo',
         country: 'Georgia',
-        audio: audio2
+        audio: audio14
     },
     {
         id: 15,
         artist: 'Lord of the Lost',
         name: 'Blood & Glitter',
         country: 'Germany',
-        audio: audio2
+        audio: audio15
     },
     {
         id: 16,
         artist: 'Victor Vernicos',
         name: 'What They Say',
         country: 'Greece',
-        audio: audio2
+        audio: audio16
     },
     {
         id: 17,
         artist: 'Diljá',
         name: 'Power',
         country: 'Iceland',
-        audio: audio2
+        audio: audio17
     },
     {
         id: 18,
         artist: 'Wild Youth',
         name: 'We Are One',
         country: 'Ireland',
-        audio: audio2
+        audio: audio18
     },
     {
         id: 19,
         artist: 'Noa Kirel',
         name: 'Unicorn',
         country: 'Israel',
-        audio: audio2
+        audio: audio19
     },
     {
         id: 20,
         artist: 'Marco Mengoni',
         name: 'Due Vite',
         country: 'Italy',
-        audio: audio2
+        audio: audio20
     },
     {
         id: 21,
         artist: 'Sudden Lights',
         name: 'Aijā',
         country: 'Latvia',
-        audio: audio2
+        audio: audio21
     },
     {
         id: 22,
         artist: 'Monika Linkytė',
         name: 'Stay',
         country: 'Lithuania',
-        audio: audio2
+        audio: audio22
     },
     {
         id: 23,
         artist: 'The Busker',
         name: 'Dance (Our Own Party)',
         country: 'Malta',
-        audio: audio2
+        audio: audio23
     },
     {
         id: 24,
         artist: 'Pasha Parfeni',
         name: 'Soarele şi Luna',
         country: 'Moldova',
-        audio: audio2
+        audio: audio24
     },
     {
         id: 25,
         artist: 'Mia Nicolai & Dion Cooper',
         name: 'Burning Daylight',
         country: 'Netherlands',
-        audio: audio2
+        audio: audio25
     },
     {
         id: 26,
         artist: 'Alessandra',
         name: 'Queen of Kings',
         country: 'Norway',
-        audio: audio2
+        audio: audio26
     },
     {
         id: 27,
         artist: 'Blanka',
         name: 'Solo',
         country: 'Poland',
-        audio: audio2
+        audio: audio27
     },
     {
         id: 28,
         artist: 'Mimicat',
         name: 'Ai Coração',
         country: 'Portugal',
-        audio: audio2
+        audio: audio28
     },
     {
         id: 29,
         artist: 'Theodor Andrei',
         name: 'D.G.T. (Off and On)',
         country: 'Romania',
-        audio: audio2
+        audio: audio29
     },
     {
         id: 30,
         artist: 'Piqued Jacks',
         name: 'Like An Animal',
         country: 'San Marino',
-        audio: audio2
+        audio: audio30
     },
     {
         id: 31,
         artist: 'Luke Black',
         name: 'Samo Mi Se Spava',
         country: 'Serbia',
-        audio: audio2
+        audio: audio31
     },
     {
         id: 32,
         artist: 'Joker Out',
         name: 'Carpe Diem',
         country: 'Slovenia',
-        audio: audio2
+        audio: audio32
     },
     {
         id: 33,
         artist: 'Blanca Paloma',
         name: 'Eaea',
         country: 'Spain',
-        audio: audio2
+        audio: audio33
     },
     {
         id: 34,
         artist: 'Loreen',
         name: 'Tattoo',
         country: 'Sweden',
-        audio: audio2
+        audio: audio34
     },
     {
         id: 35,
         artist: 'Remo Forrer',
         name: 'Watergun',
         country: 'Switzerland',
-        audio: audio2
+        audio: audio35
     },
     {
         id: 36,
         artist: 'TVORCHI',
         name: 'Heart of Steel',
         country: 'Ukraine',
-        audio: audio2
+        audio: audio36
     },
     {
         id: 37,
         artist: 'Mae Muller',
         name: 'I Wrote A Song',
         country: 'United Kingdom',
-        audio: audio2
+        audio: audio37
     }
 ]
